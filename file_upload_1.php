f03f1e450f978d91938c3498cc06a9bb4ea8c3d3
bselfridge-sh <151676079+bselfridge-sh@users.noreply.github.com>
2023-12-06 20:50:57 -0500

    Update file_upload_1.php

--- file_upload_1.php
+++ file_upload_1.php
@@ -1,5 +1,8 @@
 <?php
 // NO PHP, BUT we need to create a sub-folder called upload in the it100alt1 root to upload to.
+// run the following at the shuser@osboxes:/var/www/html/it100alt1$  prompt.
+//    mkdir /var/www/html/it100alt1/upload
+//    chmod 777 -R /var/www/html/it100alt1/upload
 ?>
 
 <!DOCTYPE html>
